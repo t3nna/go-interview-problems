0b7f9463f36fbf17cee0387424eda58b7c7af364
Ivan Melnyk <t3nna1001@gmail.com>
2026-02-03 12:25:40 +0100

    task 1: naive implementation

--- 01-first-successful-key-lookup/task.go
+++ 01-first-successful-key-lookup/task.go
@@ -12,5 +12,34 @@ type Getter interface {
 // Returns the first successful response.
 // If all requests fail, returns an error.
 func Get(ctx context.Context, getter Getter, addresses []string, key string) (string, error) {
-	return "", nil
+	res := make(chan string)
+	errorSig := make(chan error)
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	if len(addresses) == 0 {
+		return "", nil
+	}
+	for idx, address := range addresses {
+		go func() {
+			val, err := getter.Get(ctx, address, key)
+			if err != nil {
+				if idx == len(addresses)-1 {
+					errorSig <- err
+				}
+				return
+			}
+			res <- val
+		}()
+	}
+
+	select {
+	case err := <-errorSig:
+		return "", err
+	case success := <-res:
+		return success, nil
+	case <-ctx.Done():
+		return "", context.Canceled
+	}
 }
