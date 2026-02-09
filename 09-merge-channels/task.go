573921a02f8b02314a679d770230c8d9b090c7d2
Ivan Melnyk <t3nna1001@gmail.com>
2026-02-09 14:33:22 +0100

    task 9: merge channels

--- 09-merge-channels/task.go
+++ 09-merge-channels/task.go
@@ -1,5 +1,26 @@
 package main
 
+import "sync"
+
 func Merge(channels ...<-chan int) <-chan int {
-	return nil
+
+	res := make(chan int)
+
+	var wg sync.WaitGroup
+	for _, ch := range channels {
+		wg.Add(1)
+		go func(ch <-chan int) {
+			for intChan := range ch {
+				res <- intChan
+			}
+			wg.Done()
+		}(ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(res)
+	}()
+
+	return res
 }
