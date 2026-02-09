2f26642e27e65bc931017483f8d01b1175b2adfa
Ivan Melnyk <t3nna1001@gmail.com>
2026-02-09 14:31:28 +0100

    completed

--- 10-concurrent-queue/task.go
+++ 10-concurrent-queue/task.go
@@ -4,16 +4,32 @@ import "errors"
 
 var ErrQueueFull = errors.New("queue is full")
 
-type Queue struct{}
+type Queue struct {
+	ch chan int
+}
 
 func NewQueue(size int) *Queue {
-	return &Queue{}
+	return &Queue{
+		ch: make(chan int, size),
+	}
 }
 
 func (q *Queue) Push(val int) error {
+	select {
+	case q.ch <- val:
+	default:
+		return ErrQueueFull
+	}
+
 	return nil
 }
 
 func (q *Queue) Pop() int {
+	select {
+	case val := <-q.ch:
+		return val
+	default:
+		return -1
+	}
 	return 0
 }
