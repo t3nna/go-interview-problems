98a6a6a1c43ece48b69fa6fc34241c58744a58fd
Ivan Melnyk <t3nna1001@gmail.com>
2026-02-09 16:29:29 +0100

    task 11: completed

--- 11-concurrent-queue-ii/task.go
+++ 11-concurrent-queue-ii/task.go
@@ -2,24 +2,55 @@ package main
 
 import (
 	"errors"
+	"sync"
 )
 
 var ErrQueueFull = errors.New("queue is full")
 
-type Queue struct{}
+type Queue struct {
+	data []int
+	size int
+	mu   sync.RWMutex
+}
 
 func NewQueue(size int) *Queue {
-	return &Queue{}
+	return &Queue{
+		data: make([]int, 0),
+		size: size,
+	}
 }
 
 func (q *Queue) Push(val int) error {
+	q.mu.Lock()
+	defer q.mu.Unlock()
+
+	if len(q.data) >= q.size {
+		return ErrQueueFull
+	}
+	q.data = append(q.data, val)
+
 	return nil
+
 }
 
 func (q *Queue) Pop() int {
-	return -1
+	q.mu.Lock()
+	defer q.mu.Unlock()
+	if len(q.data) <= 0 {
+		return -1
+	}
+	res := q.data[0]
+	q.data = q.data[1:]
+	return res
 }
 
 func (q *Queue) Peek() int {
-	return -1
+	q.mu.RLock()
+	defer q.mu.RUnlock()
+
+	if len(q.data) <= 0 {
+		return -1
+	}
+
+	return q.data[0]
 }
